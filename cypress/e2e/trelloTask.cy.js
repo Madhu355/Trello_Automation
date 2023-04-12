7e4077cfa1a6c36f455bf55fcdb7f980f59b047f
Madhu355 <86650555+Madhu355@users.noreply.github.com>
2023-04-13 03:38:04 +0530

    Spec Update

--- cypress/e2e/trelloTask.cy.js
+++ cypress/e2e/trelloTask.cy.js
@@ -1,6 +1,7 @@
+///<reference types="Cypress" />
 import trelloPageObjects from "../support/trelloPOM";
 describe("Trello", () => {
-  const trelloPageObjects = new trelloPageObjects();
+  const trelloTest = new trelloPageObjects();
   const boardName = "Test Board 1";
   const listName1 = "My List A";
   const listName2 = "My List B";
@@ -21,24 +22,25 @@ describe("Trello", () => {
 
   it("creates a dashboard and a task", () => {
     // Create a new board
-    trelloPageObjects.getCreateBoardBtn().contains("Create new board").click();
-    trelloPageObjects.getBoardTitle().type(boardName);
-    trelloPageObjects.getCreateBoardSubmitBtn().click();
+    cy.wait(3000);
+    trelloTest.getCreateBoardBtn().contains("Create new board").click();
+    trelloTest.getBoardTitle().type(boardName);
+    trelloTest.getCreateBoardSubmitBtn().click();
     cy.contains(boardName).should("be.visible");
 
     // Create a new list A
-    trelloPageObjects.getAddListBtn1().click();
-    trelloPageObjects.getListTitle1().type(listName1 + "{enter}");
-    trelloPageObjects.getValidateListTitle1().should("contain", listName1);
+    trelloTest.getAddListBtn1().click();
+    trelloTest.getListTitle1().type(listName1 + "{enter}");
+    trelloTest.getValidateListTitle1().should("contain", listName1);
 
     // Create a new list B
-    trelloPageObjects.getListTitle2().type(listName2 + "{enter}");
-    trelloPageObjects.getValidateListTitle2().should("contain", listName2);
+    trelloTest.getListTitle2().type(listName2 + "{enter}");
+    trelloTest.getValidateListTitle2().should("contain", listName2);
 
     // Create a new task
-    trelloPageObjects.getCreateTaskBtn().click();
+    trelloTest.getCreateTaskBtn().click();
     cy.wait(2000);
-    trelloPageObjects.getTaskTitle().type(taskName + "{enter}");
+    trelloTest.getTaskTitle().type(taskName + "{enter}");
     cy.contains(taskName).should("be.visible");
 
     // Move the task to a new position
@@ -59,16 +61,12 @@ describe("Trello", () => {
       });
 
     //Logout from Trello
-    cy.get("button[data-testid='header-member-menu-button']").click();
+    cy.get(".member-avatar").click();
+    cy.get('[data-testid="profile-link"]').click();
+    cy.get('[data-testid="header-member-menu-button"]').click();
     cy.get("button[data-testid='account-menu-logout']").click();
     cy.origin("https://id.atlassian.com", () => {
       cy.get("#logout-submit").click();
-
-      //Verify logout
-      cy.get(button[(type = "submit")]).should(
-        "have.text",
-        "Sign up - it’s free!"
-      );
     });
   });
 });
